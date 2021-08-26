0e7a4ee54a23dc1d8f8b19008a513ab3b81e3aad
filipe98freitas <filipe98freitas@gmail.com>
2021-08-26 19:17:52 +0100

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -117,7 +117,7 @@ function criaFlappyBird() {
       flappyBird.velocidade = -flappyBird.pulo;
       console.log("[depois]", flappyBird.velocidade);
     },
-    gravidade: 0.05,
+    gravidade: 0.25,
     velocidade: 0,
     atualiza() {
       if (fazColisao(flappyBird, globais.chao)) {
